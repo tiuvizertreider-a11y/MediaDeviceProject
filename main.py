f16f8d03d513780463c2e386300b3bb499cb55a7
Мирон В <tiuvizertreider@gmail.com>
2026-02-05 18:06:07 +0300

    Revert "Добавил булевую переменную"
    This reverts commit c7c87ae71c125fb1fe90ee47761b1aadd271d400.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class MediaDevice(ABC):
     MIN_VOLUME = 0           # Минимальная громкость (константа)
     BATTERY_WARNING_LEVEL = 20             # Уровень предупреждения о разрядке
     manufacturer_country = "Russia"        # Страна производства (может переопределяться в дочерних классах)
-    is_active = True         # Булевая переменная, для последующего использования в обычных методах классах (экземпляра)
+
     def __init__(self, brand, model, battery_level, is_on, current_volume):
         self.brand = brand
         self.model = model
