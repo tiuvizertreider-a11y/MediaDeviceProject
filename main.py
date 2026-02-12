7881ea90295a6a0a832ffae2c62bfdf36e68a026
Мирон В <tiuvizertreider@gmail.com>
2026-02-12 19:43:25 +0300

    Реализовал инкапсуляцию для атрибутов: brand, model, battery_level, is_on, current_volume.

--- main.py
+++ main.py
@@ -5,12 +5,55 @@ class MediaDevice(ABC):
     MIN_VOLUME = 0           # Минимальная громкость (константа)
     BATTERY_WARNING_LEVEL = 20             # Уровень предупреждения о разрядке
     manufacturer_country = "Russia"        # Страна производства (может переопределяться в дочерних классах)
-    def __init__(self, brand, model, battery_level, is_on, current_volume):
+
+    def __init__(self, brand, model, battery_level, current_volume):
         self.brand = brand
         self.model = model
         self.battery_level = battery_level
-        self.is_on = is_on
+        self.is_on = False
         self.current_volume = current_volume
+
+    # Геттеры
+    @property
+    def brand(self):
+        return self.__brand
+
+    @property
+    def model(self):
+        return self.__model
+
+    @property
+    def battery_level(self):
+        return self.__battery_level
+
+    @property
+    def is_on(self):
+        return self.__is_on
+
+    @property
+    def current_volume(self):
+        return self.__current_volume
+
+    # Сеттеры
+    @brand.setter
+    def brand(self, brand):
+        self.__brand = brand
+
+    @model.setter
+    def model(self, model):
+        self.__model = model
+
+    @battery_level.setter
+    def battery_level(self, battery_level):
+        self.__battery_level = battery_level
+
+    @is_on.setter
+    def is_on(self, is_on):
+        self.__is_on = is_on
+
+    @current_volume.setter
+    def current_volume(self, current_volume):
+        self.__current_volume = current_volume
 
     # Абстрактные методы
     @abstractmethod
@@ -25,6 +68,7 @@ class MediaDevice(ABC):
     def get_device_type(self):
         pass
 
+    # Обычные методы класса (экземпляра)
     def power_on(self):
         return 'Включить устройство'
 
