e425ccb74340f99da8f441c4aafaab0b8c329f34
Павел Муслаев <92016943+PavelMuslaev@users.noreply.github.com>
2026-03-05 19:33:48 +0300

    Solve the Issue #8 https://github.com/tiuvizertreider-a11y/MediaDeviceProject/issues/8 (#9)
    Добавил класс Review для работы с отзывами и подробную документацию в класс Review.
    Класс содержит поля:
    - title, content, author, date.
    - списки плюсов (pros) и минусов (cons).
    - Добавлены docstring для класса, конструктора, всех свойств и методов.
    - Описаны параметры, возвращаемые значения, особенности обработки None.
    - Приведены к единообразию проверки индексов в remove_pro/remove_con".
    Реализованы свойства с валидацией типов, методы добавления
    и удаления пунктов. Значение date по умолчанию - текущая дата
    и время.
    ---------
    
    Co-authored-by: Мирон В <tiuvizertreider@gmail.com>

---
+++ ProjectClasses/review.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+
+
+class Review:
+    """Модель Review для работы с отзывами"""
+
+    def __init__(self, title: str, content: str, date: datetime=None,
+                 pros: list[str]=None, cons: list[str]=None, author: str='Эксперт'):
+        """
+        Инициализирует объект отзыва.
+
+        :param title:   Заголовок отзыва. Не может быть пустой строкой.
+        :param content: Содержание (текст) отзыва.
+        :param author:  Имя автора. По умолчанию 'Эксперт'.
+        :param date: Дата и время создания. Если не указана,
+                будет установлена текущая дата и время.
+        :param pros: Список преимуществ.
+                Если передан список, создаётся его копия. Если None, создаётся пустой список.
+        :param cons: Список недостатков.
+                Если передан список, создаётся его копия. Если None, создаётся пустой список.
+        """
+        self.__title = title
+        self.__content = content
+        self.__date = date if date is not None else datetime.now()
+        self.__pros = pros.copy() if pros is not None else []
+        self.__cons = cons.copy() if cons is not None else []
+        self.__author = author
+
+    # Геттеры (для чтения всех атрибутов)
+    @property
+    def title(self) -> str:
+        """Возвращает название обзора"""
+        return self.__title
+
+    @property
+    def content(self) -> str:
+        """Возвращает содержание обзора"""
+        return self.__content
+
+    @property
+    def date(self) -> datetime:
+        """Возвращает дату публикации"""
+        return self.__date
+
+    @property
+    def pros(self) -> list[str]:
+        """Возвращает список плюсов"""
+        return self.__pros.copy()
+
+    @property
+    def cons(self) -> list[str]:
+        """Возвращает список минусов"""
+        return self.__cons.copy()
+
+    @property
+    def author(self) -> str:
+        """Возвращает имя автора"""
+        return self.__author
+
+    def add_pro(self, pro_text: str) -> None:
+        """
+        Добавляет плюс в список плюсов.
+        :param pro_text: Новый плюс.
+        :return: None.
+        """
+        if len(pro_text) <= 200:
+            self.__pros.append(pro_text)
+        else:
+            print(f'Текст плюса слишком длинный')
+
+    def add_con(self, con_text: str) -> None:
+        """
+        Добавляет минус в список минусов.
+        :param con_text: Новый минус.
+        :return: None.
+        """
+        if len(con_text) <= 200:
+            self.__cons.append(con_text)
+        else:
+            print(f'Текст минуса слишком длинный')
+
+    def remove_pro(self, index: int) -> None:
+        """
+        Удаляет плюс из списка плюсов по индексу.
+        :param index: Индекс удаляемого элемента.
+        :return: None.
+        """
+        if -len(self.__pros) < index >= len(self.__pros):
+            print('Индекс вне диапазона')
+        else:
+            del self.__pros[index]
+
+    def remove_con(self, index: int) -> None:
+        """
+        Удаляет минус из списка минусов по индексу.
+        :param index: Индекс удаляемого элемента.
+        :return: None.
+        """
+        if -len(self.__cons) < index >= len(self.__cons):
+            print('Индекс вне диапазона')
+        else:
+            del self.__cons[index]
